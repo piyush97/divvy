7f14d44b8002581f899e726dbb24a050ccdd7fdc
Piyush Mehta <me@piyushmehta.com>
2021-10-18 14:29:58 +0530

    Market Smart Contract [WIP]

---
+++ contracts/Market.sol
@@ -0,0 +1,32 @@
+// SPDX-License_Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+// Security control to prevent re-entry attack
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemsIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint256 listingPrice = 0.025 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+}
