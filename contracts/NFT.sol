d968d5ff22152c8f9f30f3b720f684eae36b57fb
Piyush Mehta <me@piyushmehta.com>
2021-10-18 14:13:10 +0530

    NFT Contract made

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -9,4 +9,19 @@ contract NFS is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+
+        return newItemId;
+    }
 }
