8e5b0d8aa7fc57a4e70f18787184a07cef290c6e
Piyush Mehta <me@piyushmehta.com>
2021-10-19 11:48:21 +0530

    fetch market items

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -99,4 +99,22 @@ contract NFTMarket is ReentrancyGuard {
         _itemsSold.increment();
         payable(owner).transfer(listingPrice);
     }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint256 itemCount = _itemsIds.current();
+        uint256 unsoldItemCount = _itemsIds.current() - _itemsSold.current();
+        uint256 currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+
+        for (uint256 i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].owner == address(0)) {
+                uint256 currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
 }
