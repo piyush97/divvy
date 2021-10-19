99881793cf9fddc2a78245ab98b2d5fab083163c
Piyush Mehta <me@piyushmehta.com>
2021-10-19 10:48:54 +0530

    market sale function

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -79,4 +79,24 @@ contract NFTMarket is ReentrancyGuard {
             false
         );
     }
+
+    function createMarketSale(address nftContract, uint256 itemId)
+        public
+        payable
+        nonReentrant
+    {
+        uint256 price = idToMarketItem[itemId].price;
+        uint256 tokenId = idToMarketItem[itemId].tokenId;
+        require(
+            msg.value == price,
+            "Price submit the asking price in order to complete the purchase"
+        );
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        idToMarketItem[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
+    }
 }
