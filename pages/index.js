1b69c542c13e18d860c2cc25c4053f39154dc9b4
Piyush Mehta <me@piyushmehta.com>
2021-10-20 10:58:59 +0530

    [WIP] frontend of the marketplace

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
+import axios from "axios";
 import Head from "next/head";
+import { useEffect, useState } from "React";
+import Market from "../artifacts/contracts/Market.sol/Market.json";
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
 /**
  *  Home page
  *
@@ -7,6 +11,49 @@ import Head from "next/head";
  * @return {*}
  */
 const Home = () => {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState(false);
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  const loadNFTs = async () => {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(
+      nftmarketaddress,
+      Market.abi,
+      provider
+    );
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i, tokenId);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnit(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState(false);
+  };
+
+  if (loadingState === true && !nfts.length)
+    return (
+      <h1 className="px-20 py-10 text-3xl">No items in the Marketplace</h1>
+    );
+
   return (
     <>
       <Head>
--- utils/constants/menu.js
+++ utils/constants/menu.js
@@ -2,4 +2,5 @@ export const MENU_ITEMS = [
   { key: 0, name: "Home", link: "/" },
   { key: 1, name: "Sell Digital Asset", link: "/create-item" },
   { key: 2, name: "My digital Assets", link: "/my-assets" },
+  { key: 3, name: "Creator Dashboard", link: "/dashboard" },
 ];
