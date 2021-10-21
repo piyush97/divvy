50ba451b7f883354219f353000c2ff85fb852460
Piyush Mehta <me@piyushmehta.com>
2021-10-21 13:27:39 +0530

    [WIP] frontend of the marketplace

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ethers } from "ethers";
 import Head from "next/head";
 import { useEffect, useState } from "React";
 import Market from "../artifacts/contracts/Market.sol/Market.json";
@@ -49,6 +50,27 @@ const Home = () => {
     setLoadingState(false);
   };
 
+  const buyNFT = async (nft) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+
+    const transaction = await contract.createMarketSale(
+      nftadress,
+      nft.tokenId,
+      { value: price }
+    );
+
+    await transaction.wait();
+
+    loadNFTs();
+  };
+
   if (loadingState === true && !nfts.length)
     return (
       <h1 className="px-20 py-10 text-3xl">No items in the Marketplace</h1>
@@ -61,6 +83,25 @@ const Home = () => {
         <meta name="description" content="Made by Piyush Mehta" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="flex justify-center">
+        <div className="px-4" style={{ maxWidth: "1600px" }}>
+          <div className="grid grid-cols-1 gap-4 pt-4 sm:grid-cols-2 lg:grid-cols-4">
+            {nfts.map((nft, i) => (
+              <div className="overflow-hidden border shadow rounded-xl" key={i}>
+                <img src={nft.image} />
+                <div className="p-4">
+                  <p
+                    style={{ height: "64px" }}
+                    className="text-2xl font-semibold"
+                  >
+                    {nft.name}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
